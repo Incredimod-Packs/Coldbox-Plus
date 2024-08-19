531c89e64b5c07aaeb960aedec649c4e8ec4989c
Incredi Dexer <157266806+IncrediDexer@users.noreply.github.com>
2024-08-18 23:33:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-});
-
 const cacheName = "coldbox-plus";
 const appShellFiles = [
   "asset-v7/app.js",
@@ -23,3 +19,68 @@ const appShellFiles = [
   "/pwa-examples/js13kpwa/icons/icon-256.png",
   "/pwa-examples/js13kpwa/icons/icon-512.png",
 ];
+const gamesImages = [];
+for (let i = 0; i < games.length; i++) {
+  gamesImages.push(`data/img/${games[i].slug}.jpg`);
+}
+const contentToCache = appShellFiles.concat(gamesImages);
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })(),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
+  );
+});
+
+contentToCache.push("/pwa-examples/js13kpwa/icons/icon-32.png");
+
+// ...
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      await cache.addAll(contentToCache);
+    })(),
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        }),
+      );
+    }),
+  );
+});
